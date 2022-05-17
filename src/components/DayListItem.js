3d8e51b2bc504e4caca9e6c5142aae7f17428574
Muhammed Tariq <momotrq94@gmail.com>
2022-05-17 16:40:42 -0400

    create DayListItem tests and create function to conditionally render messages based on spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  let dayClass = classNames(
+    "day-list__item",
+    { "day-list__item--selected": props.selected },
+    { "day-list__item--full": !props.spots }
+  );
+
+  const formatSpots = (spots) => {
+    return (
+      <>
+        {spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+        {spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
+        {spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
+      </>
+    );
+  };
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {formatSpots(props.spots)}
     </li>
   );
 }
