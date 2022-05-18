45b7b226440ab619c495bf94b254d824612e72d2
Muhammed Tariq <momotrq94@gmail.com>
2022-05-18 14:37:58 -0400

    refactor InterviewList and InterviewListItem to not have to pass id down as a prop, update stories to reflect change

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,10 +7,7 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected": props.selected,
   });
   return (
-    <li
-      onClick={() => props.setInterviewer(props.id)}
-      className={interviewerClass}
-    >
+    <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
