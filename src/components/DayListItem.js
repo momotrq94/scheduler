ca47b1c90333ad8ba2da0e42798f94cbe01f4427
Muhammed Tariq <momotrq94@gmail.com>
2022-05-25 19:15:00 -0400

    install cypress, write E2E tests to create, delete and edit appointments

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,11 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       {formatSpots(props.spots)}
     </li>
