42c42010d114183a6ce3946aa166b8f6688b43fa
Muhammed Tariq <momotrq94@gmail.com>
2022-05-25 19:23:12 -0400

    code cleanup

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import axios from "axios";
 import {
   getAppointmentsForDay,
   getInterview,
